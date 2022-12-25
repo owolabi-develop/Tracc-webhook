834d76ffea934bcd9b31c0eebbea08649494969e
owolabi-develop <owolabidevelop84@gmail.com>
2022-12-25 03:31:33 -0800

    initial

---
+++ webhook/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path,include
+from .import views
+
+urlpatterns = [
+    path("",views.index,name='index'),
+    path("ca9e9764-9e0d-4318-9d6d-9b6180d195f4/",views.WebHookSetUp,name=" WebHookSetUp")
+   
+]
---
+++ webhook/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.http import *
+import json
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
+
+
+# Create your views here.
+EndPoint= 'http://localhost:1998/ca9e9764-9e0d-4318-9d6d-9b6180d195f4/'
+verify_token ="cd4ff259-f8e8-4d9e-8d69-410d1e2aed56"
+
+def index(request):
+    return HttpResponse("hello webhook")
+
+@csrf_exempt
+def WebHookSetUp(request):
+    if request.method == "GET":
+        mode = request.GET['hob.mode']
+        token = request.GET['hub.verify_token']
+        challenge = request.GET['hub.challenge']
+        if mode =="subscribe" and token == verify_token:
+            print("Verified webhook")
+            return HttpResponse(challenge,status=200)
+        else:
+            return HttpResponse('Invalid verification token',status=403)
+    if request.method == "POST":
+        data = json.loads(request.body)
+        print("Received webhook data: {} ".format(data))
+        return HttpResponse("please subscribe field object on your dasboard")
+
+
+   
